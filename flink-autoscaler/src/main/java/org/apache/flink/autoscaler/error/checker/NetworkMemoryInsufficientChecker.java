d83e00b7ca44a31239321efcd641599330da773a
Rui Fan <fanrui@apache.org>
2025-03-06 10:58:14 +0800

    Cover more exception info

--- flink-autoscaler/src/main/java/org/apache/flink/autoscaler/error/checker/NetworkMemoryInsufficientChecker.java
+++ flink-autoscaler/src/main/java/org/apache/flink/autoscaler/error/checker/NetworkMemoryInsufficientChecker.java
@@ -35,10 +35,9 @@ public class NetworkMemoryInsufficientChecker<KEY, Context extends JobAutoScaler
                         .get();
 
         for (var exceptionEntry : aa.getExceptionHistory().getEntries()) {
-            if (exceptionEntry.getStacktrace().contains("Insufficient number of network buffers")
-                    || exceptionEntry
-                            .getStacktrace()
-                            .contains("Timeout triggered when requesting exclusive buffers")) {
+            var stacktrace = exceptionEntry.getStacktrace();
+            if (stacktrace.contains("Insufficient number of network buffers")
+                    || stacktrace.contains("Timeout triggered when requesting exclusive buffers")) {
                 LOG.info(
                         "Found Insufficient number of network buffers exception, it's unrecoverable error if job isn't changed.");
                 return true;
